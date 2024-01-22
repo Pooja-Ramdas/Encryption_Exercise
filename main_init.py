192c76213f9efc13f7a714ab3da92607182eab3c
Ray <89009692+mvrck-dev@users.noreply.github.com>
2024-01-22 23:10:42 +0530

    Create main_init.py
    main_init.py is a custom python library that checks if all the correct dependencies are installed before running the program.

---
+++ main_init.py
@@ -0,0 +1,85 @@
+# This program checks if the dependencies are installed or not and if not, it installs them.
+
+import subprocess
+import sys
+import time
+
+if sys.version >= '3.9':
+    print('Your Python version is above 3.9')
+    print('Checking Packages')
+    time.sleep(2)
+else:
+    print('Your Python version is not above 3.9')
+    print('Please update your Python version')
+    time.sleep(2)
+    exit()
+
+def __init():
+
+    c = 4
+    div = 100/c
+    n = 0
+
+    try:
+        import subprocess
+        print("subprocess is installed")
+        n += div
+    except ImportError:
+        print("subprocess is not installed")
+        print("installing subprocess")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'uuid'])
+        print("subprocess is now installed")
+        import subprocess
+        n += div
+        
+    try:
+        import sys
+        print("sys is installed")
+        n += div
+    except ImportError:
+        print("sys is not installed")
+        print("installing sys")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'sys'])
+        print("sys is now installed")
+        import sys
+        n += div
+
+
+    try:
+        import hashlib
+        print("hashlib is installed")
+        n += div
+    except ImportError:
+        print("hashlib is not installed")
+        print("installing hashlib")
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", 'hashlib'])
+        print("hashlib is now installed")
+        import hashlib
+        n += div
+    print("If the code breaks down in the next step please run the program again.")
+    time.sleep(2)
+
+    try:
+        import tkinter
+        print("tkinter is installed")
+        n += div
+    except ImportError:
+        print("tkinter is not installed")
+        print("installing tkinter")
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", 'tkinter'])
+        print("tkinter is now installed")
+        import tkinter
+        n += div
+
+    if round(n) == 100:
+        print("All Packages are installed\nInitialising Encryptor[main.py]")
+    else:
+        print("Some Packages are not installed")
+        print("Please restart the program again")
+        time.sleep(2)
+        exit()
+
+
+__init()
